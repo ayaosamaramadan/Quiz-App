b6fb75cc6f3c0810e8a3d0eedb31aae587ad82da
Aya Osama <ayaramadan2011@gmail.com>
2024-07-09 00:45:34 +0300

    modif

--- quiz-app/src/componants/Time.jsx
+++ quiz-app/src/componants/Time.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 
 function Time({ dispatch, sec }) {
-  const mins= Math.floor(sec / 60);
-   const secn = sec % 60;
+  const mins = Math.floor(sec / 60);
+  const secn = sec % 60;
   useEffect(
     function () {
       const inter = setInterval(function () {
@@ -15,7 +15,7 @@ function Time({ dispatch, sec }) {
     [dispatch]
   );
   return (
-    <span className="time">
+    <span className={`time  ${mins === 0 && sec <= 15 ? "red" : ""}`}>
       0{mins}:{secn >= 0 && `${secn < 10 ? `0${secn}` : secn}`}
     </span>
   );
--- quiz-app/src/contx/Provider.jsx
+++ quiz-app/src/contx/Provider.jsx
@@ -2,56 +2,56 @@ import React, { createContext, useContext, useEffect, useReducer } from "react";
 
 const CreatedContext = createContext();
 
-  function reducer(state, action) {
-    switch (action.type) {
-      case "fetchedData":
-        return { ...state, questions: action.payload, status: "ready" };
+function reducer(state, action) {
+  switch (action.type) {
+    case "fetchedData":
+      return { ...state, questions: action.payload, status: "ready" };
 
-      case "start":
-        return { ...state, status: "active" };
+    case "start":
+      return { ...state, status: "active" };
 
-      case "reset":
-        return {
-          ...init,
-          questions: state.questions,
-          bestScore: state.bestScore,
-          status: "ready",
-        };
-      case "next":
-        return { ...state, queIndex: state.queIndex + 1, answer: null };
-      case "newAnswer":
-        const selectque = state.questions.at(state.queIndex);
-        return {
-          ...state,
-          answer: action.payload,
-          points:
-            action.payload === selectque.correctOption
-              ? state.points + selectque.points
-              : state.points,
-        };
-      case "finished":
-        return {
-          ...state,
-          status: "finish",
-          bestScore:
-            state.points > state.bestScore ? state.points : state.bestScore,
-        };
-      case "error":
-        return { ...state, status: "error" };
+    case "reset":
+      return {
+        ...init,
+        questions: state.questions,
+        bestScore: state.bestScore,
+        status: "ready",
+      };
+    case "next":
+      return { ...state, queIndex: state.queIndex + 1, answer: null };
+    case "newAnswer":
+      const selectque = state.questions.at(state.queIndex);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === selectque.correctOption
+            ? state.points + selectque.points
+            : state.points,
+      };
+    case "finished":
+      return {
+        ...state,
+        status: "finish",
+        bestScore:
+          state.points > state.bestScore ? state.points : state.bestScore,
+      };
+    case "error":
+      return { ...state, status: "error" };
 
-      case "time":
-        return {
-          ...state,
-          sec: state.sec - 1,
-          status: state.sec === 0 ? "finish" : state.status,
-        };
+    case "time":
+      return {
+        ...state,
+        sec: state.sec - 1,
+        status: state.sec === 0 ? "finish" : state.status,
+      };
 
-      default:
-        throw new Error("error in fetch data");
-    }
+    default:
+      throw new Error("error in fetch data");
   }
-  
-  function Provider({ children }) {
+}
+
+function Provider({ children }) {
   const init = {
     status: "loading",
     questions: [],
@@ -62,11 +62,9 @@ const CreatedContext = createContext();
     answer: null,
   };
 
-  
-
   const [state, dispatch] = useReducer(reducer, init);
 
-useEffect(function () {
+  useEffect(function () {
     fetch("http://localhost:5000/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "fetchedData", payload: data }))
@@ -76,7 +74,7 @@ useEffect(function () {
   const queNum = questions.length;
 
   return (
-    <CreatedContext.Provider value={{state, dispatch , queNum}}>
+    <CreatedContext.Provider value={{ state, dispatch, queNum }}>
       {children}
     </CreatedContext.Provider>
   );
